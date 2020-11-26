191b1a165e0bc20396cd3a79f3e5c6db10e0d34c
Crandel <cradlemann@gmail.com>
2020-11-27 00:41:22 +0100

    Init docker setup

--- create_containers.sh
+++ create_containers.sh
@@ -2,6 +2,6 @@
 
 docker build -t test_arch .
 
-docker run -d -it --name arch1 my_arch
-docker run -d -it --name arch2 my_arch
-docker run -d -it --name arch3 my_arch
+docker run -d -it --name arch1 test_arch
+docker run -d -it --name arch2 test_arch
+docker run -d -it --name arch3 test_arch
